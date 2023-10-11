df5a785e11f68c7292932c28e0b8cc74714d365d
Mjkey2 <Mjmarentes2005@gmail.com>
2023-10-11 15:17:55 -0700

    change

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,12 +24,13 @@ import csv
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
 #variables
-total_months = 0 # create variable to store total motnhs
-net_total = 0 #variable for TOTAL
-total_profits = 0 # profits
-total_losses = 0 # losses
-greatinc = 0 #holds the number for the greatest profit increase
-greatdec = 0 #holds the number for the greatest profit decrease
+total_months = [] # create variable to store total motnhs
+net_total = [] #variable for TOTAL
+average_change = [] #finds average change
+
+monthly_change = 0
+monthly_change_total = 0 #
+profit_counter = 0 # counts the profits :^3
 
 #open the CSV uisng the UTF-8 encoding
 with open(csvpath, encoding='UTF-8') as csvfile:
@@ -37,45 +38,30 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     header = next(csvreader)
 
     for row in csvreader:
-        profits = row[1]
-        date = row[0]
         #make statistics scores
-        attributes = {
-        #"avgchange" : 1.0 * sum(profits) / len(profits),
-        "greatinc" : max(profits),
-        "greatdec" : min(profits)
-        }
-        greatinc = attributes["greatinc"]
-        greatdec = attributes["greatdec"]
-
-        #loop through, and calculate data
-                        #for row in csvreader:  NOT NEEDED BC LINE 40 
-        total_months += 1 #counts total months
-
-        #checks b column for its numbers
-        if int(row[1]) > 0:
-            total_profits += int(row[1])
-        else : total_losses += int(row[1])
-
-        if int(row[1]) == greatinc:
-            greatincdate = row[0]
-        else : greatdecdate = row[0]
-
-        net_total = total_profits + total_losses #net gains minus net losses
-        profitaverage = total_profits + total_losses / total_months
-
-    ##TEST PRINT CSV CODE
-    #print()
+        total_months.append(row[0])
+        SumMonths = len(total_months)
+        net_total.append(float(row[1]))
+        ProfitForMonth = int(row[1])
+        monthly_change = float(ProfitForMonth)
+        monthly_change_total += monthly_change
+        profit_counter = int(row[1])
+        average_change.append(monthly_change)
+        MaxProfit = max(average_change)
+        MaxIndex = average_change.index(MaxProfit)
+        MinProfit = min(average_change)
+        MinIndex = average_change.index(MinProfit)
+        AverageChangeProfit = round(monthly_change_total/SumMonths)
+        SumAmount = sum(net_total)
 
 #output for the text file and terminal
 output = "================\n"
-output += f"Total Months :  {total_months}\n"
-output += f"Net Total : ${net_total}\n"
-output += f"Average Change : ${profitaverage}\n"
-output += f"GREATEST INCREASE : {date} (${greatinc})\n"
-output += f"GREATEST DECREASE : {date} (${greatdec})\n"
-output += f"Net Money Gained : ${total_profits}\n"
-output += f"Net Money Lossed : ${total_losses}\n"
+output += f"Total Months :  {SumMonths}\n"           ###CHECK
+output += f"Net Total : ${SumAmount}\n"                 ###CHECK
+output += f"Average Change : ${AverageChangeProfit}\n"
+output += f"GREATEST INCREASE : {total_months[MaxIndex]} (${MaxProfit})\n"
+output += f"GREATEST DECREASE : {total_months[MinIndex]} (${MinProfit})\n"
+
 output += "================"
 print(output)
 with open("./analysis/pythontxt.txt", "w") as txtfile:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,53 @@
 #PyPoll instructions!!
 
+#Create file paths across operating systems
+import os
+#module for reading csv files
+import csv
 #derive data set from election_data.csv
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+
+
+
+#create a script that analyzes and calculates the following:
+
+#Dictionary for candidates and their keys as vote count values
+Candidates = {'Charles': 0, 
+              'Diana': 0, 
+              'Raymon': 0,
+              }
+
+
+with open(csvpath,encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
 
 #create these three variables
     #voter ID
+    voter_idvar = []
     #county
+    countyvar = []
     #candidate
+    candidatevar = []
+    candidateTOTAL = []
+    #run through the datasheet and if charles has a vote per row add +1 to charles if not diana elif Raymon
+    candidatevar.append(row[2])
+    Candidates.len(candidatevar)
+    
+    #count number of instances for candidates
+    #for row in csvreader:
+        #if row[2].count("Charles"):
+            #Candidates['Charles'] + 1
 
-#create a script that analyzes and calculates the following:
-    #The total number of votes cast
+    Candidates['Charles'] + 1
+    print(Candidates['Charles'])
+#calculate the percentages. Sum all three and then find the percentages for the three
+
+
+#The total number of votes cast
+#count_votes.append(row[0])
+#sumvotes = len(count_votes)
 
     #A complete list of candidates who received votes
 
@@ -17,8 +56,8 @@
     #The total number of votes each candidate won
 
     #The winner of the election based on popular vote
-
-#Analysis should align with the following results
+        
+###Analysis should align with the following results
     #Election Results
     
     #Total Votes: 369711
@@ -30,3 +69,20 @@
     #Winner: Diana DeGette
 
 #the final script should both print the analysis to the terminal and export a TEXT FILE with the results
+
+
+#output = "================\n"
+#output += "Election Results\n"
+#output += "----------------\n"
+#output += f"Total Votes: {votetotal}\n"
+#output += "----------------\n"
+#output += f"{Charles alphabetical order} \\\PERCENT AND TOTAL COUNT \n"
+#output += f"{Diana alphabetical order} \\\PERCENT AND TOTAL COUNT \n"
+#output += f"{Raymon alphabetical order} \\\PERCENT AND TOTAL COUNT \n"
+#output += "----------------\n"
+#output += "Winner :" + winner \n"
+#output += "================"
+
+#print(output)
+#with open("./analysis/pythonpolltxt.txt", "w") as txtfile:
+    #txtfile.write(output)
